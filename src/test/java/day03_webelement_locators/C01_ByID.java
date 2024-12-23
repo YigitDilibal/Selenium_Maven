f226f00f2f68a91ddbbb75fd04569b5283e6aa66
YigitDilibal <archery10@outlook.com>
2024-12-23 20:15:13 +0200

    3. ders

---
+++ src/test/java/day03_webelement_locators/C01_ByID.java
@@ -0,0 +1,41 @@
+package day03_webelement_locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_ByID {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+        // TestOtomasyonu anasayfaya gidin.
+        driver.get("https://www.testotomasyonu.com/");
+
+        // arama kutusunu locate edip kaydedin
+        WebElement aramaKutusu = driver.findElement(By.id("global-search"));
+
+        // arama kutusuna phone yazdirin
+        aramaKutusu.sendKeys("phone");
+
+        //arama yapilmasi icin ENTER tusuna basin
+        aramaKutusu.submit();
+
+        //sayfayi kapatin
+        Thread.sleep(2000);
+        driver.quit();
+
+
+
+
+
+    }
+
+}
